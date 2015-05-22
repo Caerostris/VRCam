0547ad4ff7df272b57635f7004e5cf1f4c225623
Keno Schwalb <keno@kenoschwalb.com>
2015-05-22 11:23:45 +0100

    initial commit

---
+++ Assets/Cardboard/DemoScene/CameraView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraView : MonoBehaviour {
+	WebCamTexture webcamTexture;
+
+	// Use this for initialization
+	void Start () {
+		// initialise the front camera and map it to the plane
+		webcamTexture = new WebCamTexture ();
+		GetComponent<Renderer> ().material.mainTexture = webcamTexture;
+		webcamTexture.Play ();
+
+		Color32[] pixels = webcamTexture.GetPixels32 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Cardboard/DemoScene/DisableFog.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableFog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RenderSettings.fogStartDistance = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
